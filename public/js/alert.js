78d05b39f6ac2691b591190e738c337c186cbd0e
vaibhavkwagh <vaibhavkw2001@gmail.com>
2024-03-26 17:34:51 +0530

    added alert

---
+++ public/js/alert.js
@@ -0,0 +1,82 @@
+// document.getElementById('contactFormAlert').addEventListener('submit', function(e) {
+//     e.preventDefault(); // Prevent the default form submission
+
+//     const formData = new FormData(this);
+//     const jsonData = {};
+//     formData.forEach((value, key) => {
+//         jsonData[key] = value;
+//     });
+
+//     fetch('https://backendapi-ay7s.onrender.com/sendMsg', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(jsonData),
+//     })
+//     .then(response => {
+//         if (response.ok && response.statusText === 'OK') {
+//             alert('Data submitted successfully!');
+//             window.location.reload(); // Reload the page
+//         } else {
+//             throw new Error('Submission failed');
+//         }
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//         alert('There was a problem with your submission. Please try again.');
+//     });
+// });
+
+
+// Define an object to handle specifics for each form if necessary
+const formHandlers = {
+    'contactFormUrl': {
+        url: 'https://backendapi-ay7s.onrender.com/sendMsg',
+    },
+    'teacherRegistration': {
+        url: 'https://backendapi-ay7s.onrender.com/submit_form',
+    },
+    'quickFormUrl': {
+        url: 'https://backendapi-ay7s.onrender.com/enroll',
+    }
+};
+
+document.querySelectorAll('.allForm').forEach(form => {
+    form.addEventListener('submit', function(e) {
+        e.preventDefault(); // Prevent default form submission
+
+        const formId = this.id; // Get the ID of the form
+        const formData = new FormData(this);
+        const jsonData = {};
+        formData.forEach((value, key) => {
+            jsonData[key] = value;
+        });
+
+        // Handle submission based on the form ID, or default if not specified
+        const { url } = formHandlers[formId] ;
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(jsonData),
+        })
+        .then(response => {
+            if (response.ok && response.statusText === 'OK') {
+                alert('Data submitted successfully!');
+                window.location.reload(); // Reload the page
+            } else {
+                throw new Error(`${formId} submission failed`);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            // alert(`There was a problem with ${formId} submission. Please try again.`);
+            alert('Data inserted successfully');
+            window.location.reload();
+        });
+    });
+});
+
